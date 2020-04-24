e4ac1ed02829deaffa7eb1b04286f2bad525d91f
Leonardo <lvenk26@gmail.com>
2020-04-24 00:08:15 -0300

    Working ship spawner

--- src/game.rs
+++ src/game.rs
@@ -1,46 +1,81 @@
-use sa;
+use std::f64;
+use crate::config::*;
+use crate::ship::*;
 use piston_window::*;
+use rand::*;
 
-// Constants
-const W_WIDTH : f64 = 480.0;
-const W_HEIGHT: f64 = 640.0;
-const W_RES : (f64, f64) = (W_WIDTH, W_HEIGHT);
+/// Checks if a new ship can be spawned
+pub fn can_spawn(ships: &Vec<Ship>) -> bool {
+    if ships.len() == 0 {
+        return true;
+    } else if ships.len() >= MAX_SHIPS {
+        return false;
+    }
 
-// Make sure that we can divide the screen correctly
-sa::const_assert!(W_WIDTH as usize % 3 == 0);
+    // find the ship closest to the top of the screen
+    let mut dist = f64::MAX;
+    let mut last_ship = Option::<&Ship>::None;
 
-const LANE_WIDTH : f64 = W_WIDTH / 3.0;
-// Where each lane starts and ends
-// |          |          |          |
-// 0         10         20         30
-const LANES : [f64; 4] = [0.0, LANE_WIDTH, LANE_WIDTH * 2.0, LANE_WIDTH * 3.0];
+    for ship in ships.iter() {
+        if ship.progress < dist {
+            last_ship = Some(&ship);
+            dist = ship.progress;
+        }
+    }
 
-// Colors
-const SHIP_COLOR: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
-
-const BG_COLOR : [f32; 4] = [0.8; 4];
-const MARGIN : f64 = 10.0;
+    match last_ship {
+        Some(last_ship) => {
+            if last_ship.progress > SHIP_HEIGHT * GAME_DIFFICULTY {
+                return true;
+            }
+            false
+        }
+        None => return false
+    }
+}
 
 pub fn run() {
-    println!("Lane Width -> {:?}", LANE_WIDTH);
-    println!("Lane Width Boundaries -> {:?}", LANES);
-    println!("Possible Lane Width -> {:?}", LANE_WIDTH - 2.0 * MARGIN);
-
     let mut window: PistonWindow = WindowSettings::new("Brick Game", Size::from(W_RES))
-        .resizable(true)
+        .resizable(false)
         .exit_on_esc(true)
         .build()
         .unwrap();
 
-    while let Some(event) = window.next() {
+    let mut ships: Vec<Ship> = vec![];
+    let mut rng = rand::thread_rng();
+
+    for i in 0..N_LANES {
+        ships.push(Ship::new(i, SHIP_DEFAULT_SPEED));
+    }
+
+    while let Some(e) = window.next() {
+        if can_spawn(&ships) {
+            // Pick a lane and rotate
+            let lane: usize = rng.gen_range(0, N_LANES);
+            ships.push(Ship::new(lane, SHIP_DEFAULT_SPEED));
+        }
+
+        // update ships progress
+        if let Some(u) = e.update_args() {
+            for ship in ships.iter_mut() {
+                ship.forward(u.dt);
+            }
+        }
+
+        ships = ships
+            .into_iter()
+            .filter(|ship| !ship.is_outside())
+            .collect();
+
         // context, graphics, device
-        window.draw_2d(&event, |c, g, _d| {
+        window.draw_2d(&e, |c, g, _d| {
             clear(BG_COLOR, g);
-            // params
-            // [r, g, b, a], [x, y, w, h], t, g
-            rectangle(SHIP_COLOR, [LANES[0] + MARGIN, MARGIN, LANE_WIDTH - 2.0 * MARGIN, LANE_WIDTH], c.transform, g);
-            rectangle(SHIP_COLOR, [LANES[1] + MARGIN, MARGIN, LANE_WIDTH - 2.0 * MARGIN, LANE_WIDTH], c.transform, g);
-            rectangle(SHIP_COLOR, [LANES[2] + MARGIN, MARGIN, LANE_WIDTH - 2.0 * MARGIN, LANE_WIDTH], c.transform, g);
+            for ship in ships.iter() {
+                for rect in ship.get_parts().iter() {
+                    // [r, g, b, a], [x, y, w, h], t, g
+                    rectangle(SHIP_COLOR, *rect, c.transform, g);
+                }
+            }
         });
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@ extern crate piston_window;
 extern crate static_assertions as sa;
 
 mod agent;
+mod config;
 mod game;
+mod ship;
 
 fn main() {
     game::run();
